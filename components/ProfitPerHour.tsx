074e7c010ae127b6655dfc1e2e11f2db55a04386
khushig8 <khushigarg9785@gmail.com>
2024-09-03 13:46:10 +0530

    setting

--- components/ProfitPerHour.tsx
+++ components/ProfitPerHour.tsx
@@ -1,11 +1,11 @@
 "use client"; 
 
-import Image from "next/image";
-import AstroIcon from "../icons/Astro Circle.png";
+import Image, { StaticImageData } from "next/image";
 import Coin from "../images/coin.png";
 import SettingIcon from "../icons/Gear.svg";
 import { useState } from "react";
 import Setting from "./Setting";
+import AvalancheIcon from "../images/Blockchains/Avalanche.svg"; // Default icon or one of your choice
 
 // Define the prop types
 interface ProfitPerHourProps {
@@ -14,18 +14,28 @@ interface ProfitPerHourProps {
 
 export default function ProfitPerHour({ pointsPerHour }: ProfitPerHourProps) {
   const [showSettingPage, setShowSettingPage] = useState(false);
+  const [selectedExchanger, setSelectedExchanger] = useState<string>("Avalanche");
+  const [selectedExchangerIcon, setSelectedExchangerIcon] = useState<string | StaticImageData>(AvalancheIcon);
 
   const handleSettingClick = () => {
     setShowSettingPage(true);
   };
+  
   const handleClose = () => {
     setShowSettingPage(false);
   };
+
+  const handleExchangerChange = (name: string, icon: StaticImageData) => {
+    setSelectedExchanger(name);
+    setSelectedExchangerIcon(icon);
+    setShowSettingPage(false);
+  };
+
   return (
     <div className="px-4 w-full">
       <div className="flex items-center justify-around rounded-full border border-zinc-500 bg-neutral-800 h-full p-2">
         <div className="w-1/3 flex items-center justify-center border-r-2 border-zinc-500">
-          <Image src={AstroIcon} width={34} height={34} alt="Astro Icon" className="rounded-full" />
+          <Image src={selectedExchangerIcon} width={34} height={34} alt="Exchanger Icon" className="rounded-full" />
         </div>
         <div className="w-1/3 flex items-center justify-center border-r-2 border-zinc-500">
           <div className="flex flex-col">
@@ -37,10 +47,16 @@ export default function ProfitPerHour({ pointsPerHour }: ProfitPerHourProps) {
           </div>
         </div>
         <div className="w-1/3 flex items-center justify-center">
-          <Image src={SettingIcon} width={24} height={24} alt="Settings Icon" className="rounded-full" onClick={handleSettingClick} />
+          <Image src={SettingIcon} width={24} height={24} alt="Settings Icon" className="rounded-full cursor-pointer" onClick={handleSettingClick} />
         </div>
       </div>
-      {showSettingPage && <Setting onClose={handleClose} />}
+      {showSettingPage && (
+        <Setting
+          onClose={handleClose}
+          onExchangerChange={handleExchangerChange}
+          selectedExchanger={selectedExchanger}  // Pass down the selected exchanger
+        />
+      )}
     </div>
   );
 }
--- components/Setting.tsx
+++ components/Setting.tsx
@@ -1,12 +1,45 @@
-import React from "react";
-import Image from "next/image";
+import React, { useState } from "react";
+import Image, { StaticImageData } from "next/image";
 import Arrow from "../icons/Arrow.png";
+
+// Import your icons
+import AvalancheIcon from "../images/Blockchains/Avalanche.svg";
+import BitcoinIcon from "../images/Blockchains/Bitcoin.svg";
+import BNBChainIcon from "../images/Blockchains/BNB Chain.svg";
+import CardanoIcon from "../images/Blockchains/Cardano.svg";
+import COREDaoIcon from "../images/Blockchains/CORE Dao.svg";
+import CosmosIcon from "../images/Blockchains/Cosmos.svg";
+import EthereumIcon from "../images/Blockchains/Ethereum.svg";
+import NEARIcon from "../images/Blockchains/NEAR.svg";
+import NextGenIcon from "../images/Blockchains/NextGen Chain.svg";
+import PolkadotIcon from "../images/Blockchains/Polkadot.svg";
+import SolanaIcon from "../images/Blockchains/Solana.svg";
+import TronIcon from "../images/Blockchains/Tron.svg";
 
 interface SettingProps {
     onClose: () => void;
+    onExchangerChange: (name: string, icon: StaticImageData) => void;
+    selectedExchanger: string; // New prop for displaying the selected exchanger
 }
 
-const Setting: React.FC<SettingProps> = ({ onClose }) => {
+const exchangers = [
+    { name: "Avalanche", icon: AvalancheIcon },
+    { name: "Bitcoin", icon: BitcoinIcon },
+    { name: "BNB Chain", icon: BNBChainIcon },
+    { name: "Cardano", icon: CardanoIcon },
+    { name: "CORE Dao", icon: COREDaoIcon },
+    { name: "Cosmos", icon: CosmosIcon },
+    { name: "Ethereum", icon: EthereumIcon },
+    { name: "NEAR", icon: NEARIcon },
+    { name: "NextGen", icon: NextGenIcon },
+    { name: "Polkadot", icon: PolkadotIcon },
+    { name: "Solana", icon: SolanaIcon },
+    { name: "Tron", icon: TronIcon },
+];
+
+const Setting: React.FC<SettingProps> = ({ onClose, onExchangerChange, selectedExchanger }) => {
+    const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
+
     return (
         <div className="fixed inset-0 bg-black flex flex-col z-50 overflow-auto">
             <div className="flex flex-row items-center justify-between w-full px-4 py-4">
@@ -17,10 +50,43 @@ const Setting: React.FC<SettingProps> = ({ onClose }) => {
             </div>
             {/* Settings Options */}
             <div className="flex flex-col space-y-4 px-4 mt-4">
-                {/* <div className="flex justify-between items-center bg-zinc-800 px-4 py-3 rounded-lg">
+                <div className="relative flex bg-zinc-800 px-4 py-3 rounded-lg justify-between items-center">
                     <span className="text-white text-lg">Select Exchanger</span>
-                    <span className="text-gray-400 text-lg">Binance</span>
-                </div> */}
+                    <div className="relative">
+                    <button
+                        className="text-gray-400 text-lg flex items-center"
+                        onClick={() => setDropdownOpen(!dropdownOpen)}
+                    >
+                        {selectedExchanger}
+                        <span className={`ml-2 transform transition-transform ${dropdownOpen ? "rotate-180" : ""}`}>
+                            ▼
+                        </span>
+                    </button>
+                    {dropdownOpen && (
+                        <div className="absolute right-0 mt-2 w-48 bg-zinc-700 rounded-md shadow-lg z-50">
+                            {exchangers.map((exchanger) => (
+                                <button
+                                    key={exchanger.name}
+                                    onClick={() => {
+                                        onExchangerChange(exchanger.name, exchanger.icon);
+                                        setDropdownOpen(false);
+                                    }}
+                                    className="flex items-center w-full px-4 py-2 text-white hover:bg-zinc-600"
+                                >
+                                    <Image
+                                        src={exchanger.icon}
+                                        width={20}
+                                        height={20}
+                                        alt={exchanger.name}
+                                        className="mr-2"
+                                    />
+                                    {exchanger.name}
+                                </button>
+                            ))}
+                        </div>
+                    )}
+                     </div>
+                </div>
                 <div className="flex justify-between items-center bg-zinc-800 px-4 py-3 rounded-lg">
                     <span className="text-white text-lg">Delete Account</span>
                     <span className="text-gray-400 bg-red-600 px-1 rounded-md text-lg">Delete</span>
