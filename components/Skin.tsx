ba2431842867358ec3b9355e80209aa6f368c532
khushig8 <khushigarg9785@gmail.com>
2024-09-03 12:58:01 +0530

    skin changed

--- components/Skin.tsx
+++ components/Skin.tsx
@@ -1,31 +1,31 @@
 import React, { useState, useEffect } from "react";
 import Arrow from "../icons/Arrow.png";
 import Image, { StaticImageData } from "next/image";
-import Default from "../images/Armadillo_2.svg";
-import AstroGirl_Normal_1 from "../images/Skins/AstroGirl_SVG/AstroGirl_Normal_1.svg";
-import AstroGirl_Normal_2 from "../images/Skins/AstroGirl_SVG/AstroGirl_Normal_2.svg";
-import AstroGirl_Normal_3 from "../images/Skins/AstroGirl_SVG/AstroGirl_Normal_3.svg";
-import AstroGirl_Normal_4 from "../images/Skins/AstroGirl_SVG/AstroGirl_Normal_4.svg";
-import AstroGirl_Normal_5 from "../images/Skins/AstroGirl_SVG/AstroGirl_Normal_5.svg";
-import Panda_Normal_1 from "../images/Skins/Panda_SVG/Panda_Normal_1.svg";
-import Panda_Normal_2 from "../images/Skins/Panda_SVG/Panda_Normal_2.svg";
-import Panda_Normal_3 from "../images/Skins/Panda_SVG/Panda_Normal_3.svg";
-import Panda_Normal_4 from "../images/Skins/Panda_SVG/Panda_Normal_4.svg";
-import Panda_Normal_5 from "../images/Skins/Panda_SVG/Panda_Normal_5.svg";
-import Anne_Normal_1 from "../images/Skins/Anne_SVG/Anne_Normal_1.svg";
-import Anne_Normal_2 from "../images/Skins/Anne_SVG/Anne_Normal_2.svg";
-import Anne_Normal_3 from "../images/Skins/Anne_SVG/Anne_Normal_3.svg";
-import Anne_Normal_4 from "../images/Skins/Anne_SVG/Anne_Normal_4.svg";
-import Anne_Normal_5 from "../images/Skins/Anne_SVG/Anne_Normal_5.svg";
-import SPF_Normal_1 from "../images/Skins/SPF_SVG/SPF_Normal_1.svg";
-import SPF_Normal_2 from "../images/Skins/SPF_SVG/SPF_Normal_2.svg";
-import SPF_Normal_3 from "../images/Skins/SPF_SVG/SPF_Normal_3.svg";
-import SPF_Normal_4 from "../images/Skins/SPF_SVG/SPF_Normal_4.svg";
-import SPF_Normal_5 from "../images/Skins/SPF_SVG/SPF_Normal_5.svg";
+import Default from "../images/Skins/Astro_SVG/Normal/Armadillo_1.svg";
+import AstroGirl_Normal_1 from "../images/Skins/AstroGirl_SVG/Normal/AstroGirl_Normal_2.svg";
+import Panda_Normal_1 from "../images/Skins/Panda_SVG/Normal/Panda_Normal_3.svg";
+import Anne_Normal_1 from "../images/Skins/Anne_SVG/Normal/Anne_Normal_1.svg";
+import SPF_Normal_1 from "../images/Skins/SPF_SVG/Normal/SPF_Normal_4.svg";
+import Kayopo_Hunter_Normal from "../images/Skins/Red_Hunter_SVG/Normal/Red_Hunter_Normal_4.svg";
 import Lock from "../icons/Lock.svg";
 import UserInfo from "./UserInfo";
 
-type SkinName = "Default" | "AstroGirl_Normal_1" | "AstroGirl_Normal_2" | "AstroGirl_Normal_3" | "AstroGirl_Normal_4" | "AstroGirl_Normal_5" | "Panda_Normal_1" | "Panda_Normal_2" | "Panda_Normal_3" | "Panda_Normal_4" | "Panda_Normal_5" | "Anne_Normal_1" | "Anne_Normal_2" | "Anne_Normal_3" | "Anne_Normal_4" | "Anne_Normal_5" | "SPF_Normal_1" | "SPF_Normal_2" | "SPF_Normal_3" | "SPF_Normal_4" | "SPF_Normal_5";
+// Define a type for skin information
+type SkinName =
+  | "Default"
+  | "AstroGirl_Normal_1"
+  | "Panda_Normal_1"
+  | "Anne_Normal_1"
+  | "SPF_Normal_1"
+  | "Kayopo_Hunter_Normal";
+
+interface SkinInfo {
+  image: StaticImageData;
+  title: string;
+  subtitle: string;
+  description: string;
+  price: number;
+}
 
 interface SkinProps {
   GalacticGoldRush: StaticImageData;
@@ -40,39 +40,56 @@ interface SkinProps {
   onClose: () => void;
 }
 
-interface SkinImages {
-  [tier: string]: StaticImageData[];
-}
-
-const skinPrices: { [key in SkinName]: number } = {
-  Default: 0,
-  AstroGirl_Normal_1: 100,
-  AstroGirl_Normal_2: 200,
-  AstroGirl_Normal_3: 300,
-  AstroGirl_Normal_4: 400,
-  AstroGirl_Normal_5: 500,
-  Panda_Normal_1: 100,
-  Panda_Normal_2: 200,
-  Panda_Normal_3: 300,
-  Panda_Normal_4: 400,
-  Panda_Normal_5: 500,
-  Anne_Normal_1: 100,
-  Anne_Normal_2: 200,
-  Anne_Normal_3: 300,
-  Anne_Normal_4: 400,
-  Anne_Normal_5: 500,
-  SPF_Normal_1: 100,
-  SPF_Normal_2: 200,
-  SPF_Normal_3: 300,
-  SPF_Normal_4: 400,
-  SPF_Normal_5: 500,
-};
-
-const skinImages: SkinImages = {
-  AstroGirl_Normal: [AstroGirl_Normal_1, AstroGirl_Normal_2, AstroGirl_Normal_3, AstroGirl_Normal_4, AstroGirl_Normal_5],
-  Panda_Normal: [Panda_Normal_1, Panda_Normal_2, Panda_Normal_3, Panda_Normal_4, Panda_Normal_5],
-  Anne_Normal: [Anne_Normal_1, Anne_Normal_2, Anne_Normal_3, Anne_Normal_4, Anne_Normal_5],
-  SPF_Normal: [SPF_Normal_1, SPF_Normal_2, SPF_Normal_3, SPF_Normal_4, SPF_Normal_5],
+// Skin data structure
+const skinsData: { [key in SkinName]: SkinInfo } = {
+  Default: {
+    image: Default,
+    title: "ASTRO ARMADILLO",
+    subtitle: "The Tech-Savvy Adventurer",
+    description:
+      "Astro Armadillo is your go-to tech guru with a tough shell and a heart full of curiosity. Armed with clever gadgets and a sharp mind, he embarks on epic intergalactic adventures, solving cosmic puzzles and teaching valuable lessons about teamwork and friendship. His friendly, determined nature makes him a fan-favorite hero, showing that even the smallest creatures can make a big impact across the galaxy.",
+    price: 0,
+  },
+  AstroGirl_Normal_1: {
+    image: AstroGirl_Normal_1,
+    title: "ASTRO GIRL",
+    subtitle: "The Sophisticated Navigator",
+    description:
+      "Astro Girl is a Celestara lavish planet's savvy and charming local, deeply knowledgeable about its newfound wealth. With her wit and charisma, she guides Astro through this strange new world, becoming an essential ally in unraveling the planet’s crypto mysteries. Astro Girl is more than just a sidekick; she’s a vital part of the adventure.",
+    price: 800,
+  },
+  Panda_Normal_1: {
+    image: Panda_Normal_1,
+    title: "PINK PANDA",
+    subtitle: "The Tough Leader",
+    description:
+      "Pink Panda is the enigmatic leader of an underground resistance, channeling serious mafia don vibes. Rugged and intimidating in his pink spacesuit, he rules his hidden tech base with a mix of charm and menace. With a smirk and a toothpick, Pink Panda speaks in riddles and offers help—but always at a price. Beneath his tough exterior lies a strategic mind committed to shaping the galaxy’s future with the NextGen Chain.",
+    price: 50000,
+  },
+  Anne_Normal_1: {
+    image: Anne_Normal_1,
+    title: "ANNE",
+    subtitle: "The Loyal Mechanic",
+    description:
+      "Anne is the dependable and no-nonsense programmer and mechanic on Astro’s team. With a heart of gold and a knack for problem-solving, she keeps the spaceship running smoothly and is always ready with valuable insights when things get tough. Anne’s unwavering loyalty to Astro makes her the backbone of the team, especially in the most challenging situations.",
+    price: 10000,
+  },
+  SPF_Normal_1: {
+    image: SPF_Normal_1,
+    title: "SCAM PRANKMAN FRIED (SPF)",
+    subtitle: "The Flamboyant Villain",
+    description:
+      "SPF is the eccentric and flashy CEO behind a new exchange, embodying greed and chaos. This flamboyant pig, with his wild style and insatiable thirst for wealth, is the mastermind of the crypto rug-pull scheme causing havoc on the planet. SPF’s larger-than-life personality and cunning nature make him a memorable and menacing antagonist.",
+    price: 15000,
+  },
+  Kayopo_Hunter_Normal: {
+    image: Kayopo_Hunter_Normal,
+    title: "KAYOPO HUNTER",
+    subtitle: "The Tribal Guardian",
+    description:
+      "The Kayopo Hunter is a skilled warrior from the Mẽbêngôkre, known as the Kayapó, deeply connected to the Amazon and Cerrado regions. Master hunters and navigators, they protect their land with a deep sense of tradition and honor. Adorned in intricate body paint that reflects their cultural heritage, the Kayopo Hunter stands as a powerful symbol of strength and guardianship in the game.",
+    price: 25000,
+  },
 };
 
 const Skin: React.FC<SkinProps> = ({
@@ -87,35 +104,35 @@ const Skin: React.FC<SkinProps> = ({
   calculateProgress,
   onClose,
 }) => {
-  const [selectedSkin, setSelectedSkin] = useState<StaticImageData>(GalacticGoldRush);
+  const [selectedSkin, setSelectedSkin] = useState<SkinName>("Default");
   const [ownedSkins, setOwnedSkins] = useState<Set<SkinName>>(new Set<SkinName>(["Default"]));
   const [pendingPurchase, setPendingPurchase] = useState<SkinName | null>(null);
   const [showPopup, setShowPopup] = useState<boolean>(false);
 
   useEffect(() => {
-    const savedSkin = localStorage.getItem('selectedSkin');
-    if (savedSkin) {
-      setSelectedSkin(JSON.parse(savedSkin));
+    const savedSkinName = localStorage.getItem("selectedSkinName");
+    if (savedSkinName) {
+      setSelectedSkin(savedSkinName as SkinName);
     }
 
-    const savedOwnedSkins = localStorage.getItem('ownedSkins');
+    const savedOwnedSkins = localStorage.getItem("ownedSkins");
     if (savedOwnedSkins) {
       setOwnedSkins(new Set(JSON.parse(savedOwnedSkins)));
     }
   }, []);
 
   useEffect(() => {
-    localStorage.setItem('selectedSkin', JSON.stringify(selectedSkin));
-    localStorage.setItem('ownedSkins', JSON.stringify(Array.from(ownedSkins)));
+    localStorage.setItem("selectedSkinName", selectedSkin);
+    localStorage.setItem("ownedSkins", JSON.stringify(Array.from(ownedSkins)));
   }, [selectedSkin, ownedSkins]);
 
-  const handleSkinSelect = (skin: StaticImageData, skinName: SkinName, price: number) => {
-    setSelectedSkin(skin);
+  const handleSkinSelect = (skinName: SkinName) => {
+    setSelectedSkin(skinName);
     setPendingPurchase(ownedSkins.has(skinName) ? null : skinName);
   };
 
   const handlePurchase = () => {
-    if (pendingPurchase && userPoints >= skinPrices[pendingPurchase]) {
+    if (pendingPurchase && userPoints >= skinsData[pendingPurchase].price) {
       setShowPopup(true);
     } else if (pendingPurchase) {
       alert("You do not have enough points to purchase this skin.");
@@ -123,10 +140,10 @@ const Skin: React.FC<SkinProps> = ({
   };
 
   const handleConfirmPurchase = () => {
-    if (pendingPurchase && userPoints >= skinPrices[pendingPurchase]) {
-      setUserPoints(prev => prev - skinPrices[pendingPurchase]);
-      setOwnedSkins(prev => new Set([...Array.from(prev), pendingPurchase])); // Convert Set to Array
-      setGalacticGoldRush(selectedSkin);
+    if (pendingPurchase && userPoints >= skinsData[pendingPurchase].price) {
+      setUserPoints((prev) => prev - skinsData[pendingPurchase].price);
+      setOwnedSkins((prev) => new Set([...Array.from(prev), pendingPurchase])); // Add to owned skins
+      setGalacticGoldRush(skinsData[pendingPurchase].image);
       setPendingPurchase(null);
       setShowPopup(false);
     }
@@ -136,7 +153,7 @@ const Skin: React.FC<SkinProps> = ({
     setShowPopup(false);
   };
 
-  const isActiveSkin = selectedSkin === GalacticGoldRush;
+  const isActiveSkin = selectedSkin === "Default";
 
   return (
     <div className="fixed inset-0 bg-black flex flex-col z-50 overflow-auto">
@@ -150,24 +167,24 @@ const Skin: React.FC<SkinProps> = ({
 
       <div className="flex flex-1">
         {/* Left Sidebar for Skins Menu */}
-        <div className="w-1/2 flex flex-col items-start justify-start bg-gray-900 p-4">
+        <div className="w-1/2 flex flex-col items-start justify-start bg-gray-900 p-2">
           <button className="text-white text-center mb-4">Skin</button>
           {/* Display Selected Skin */}
           <div className="flex flex-col items-center">
-            <Image src={selectedSkin} width={200} height={200} alt="Selected Skin" />
+            <Image src={skinsData[selectedSkin].image} width={200} height={200} alt="Selected Skin" />
             <div className="text-center bg-gray-800 p-2 rounded-lg text-white mt-4">
-              <h4 className="text-lg font-bold">Anne</h4>
-              <p className="text-xs text-justify">
-                Anne, the trusted programmer and mechanic, is a dependable problem-solver with a heart of gold.
-                Her practical approach ensures the spaceship runs smoothly, and her loyalty to Astro shines through in her unwavering support during tough situations.
-              </p>
+              <h4 className="text-sm font-bold">{skinsData[selectedSkin].title}</h4>
+              <p className="text-xs text-justify">{skinsData[selectedSkin].subtitle}</p>
+              <p className="text-xs text-justify mt-2">{skinsData[selectedSkin].description}</p>
               <p className="text-xl font-bold mt-2">
-                <span role="img" aria-label="coin">🪙</span> 1,000
+                <span role="img" aria-label="coin">🪙</span> {skinsData[selectedSkin].price.toLocaleString()}
               </p>
             </div>
             <button
-              onClick={pendingPurchase ? handlePurchase : () => setGalacticGoldRush(selectedSkin)}
-              className="mt-4 px-6 py-2 rounded-full bg-purple-500 text-white"
+              onClick={pendingPurchase ? handlePurchase : () => setGalacticGoldRush(skinsData[selectedSkin].image)}
+              className={`mt-4 px-6 py-2 rounded-full text-white ${
+                isActiveSkin ? "bg-gray-500 cursor-not-allowed" : "bg-purple-500"
+              }`}
               disabled={isActiveSkin}
             >
               {pendingPurchase ? "Buy" : "Choose"}
@@ -176,35 +193,31 @@ const Skin: React.FC<SkinProps> = ({
         </div>
 
         {/* Main Content Area */}
-        <div className="w-1/2 flex flex-col items-center justify-start p-4">
+        <div className="w-1/2 flex flex-col items-center justify-start p-2">
           <button className="text-white">All</button>
           {/* List of Skins */}
-          <div className="grid grid-cols-2 gap-4 mt-6">
-            {Object.entries(skinImages).map(([tier, skins]) =>
-              skins.map((skin: StaticImageData, index: number) => {
-                const skinName = `${tier}_${index + 1}` as SkinName; // Ensure consistent format
-                const price = skinPrices[skinName];
-                const isOwned = ownedSkins.has(skinName);
-
-                return (
-                  <div
-                    key={skinName}
-                    className={`relative bg-gray-700 p-2 rounded-lg cursor-pointer ${
-                      isOwned ? "border-2 border-blue-500" : "border-2 border-gray-500"
-                    }`}
-                    onClick={() => handleSkinSelect(skin, skinName, price)}
-                  >
-                    <Image src={skin} width={50} height={100} alt={skinName} />
-                    {!isOwned && (
-                      <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
-                        <Image src={Lock} width={10} height={10} alt="Locked" />
-                      </div>
-                    )}
-                    {isActiveSkin && <div className="absolute top-0 right-0 bg-blue-500 text-white text-xs rounded-full p-1">✓</div>}
-                  </div>
-                );
-              })
-            )}
+          <div className="grid grid-cols-2 gap-2 mt-6">
+            {Object.keys(skinsData).map((skinName) => {
+              const isOwned = ownedSkins.has(skinName as SkinName);
+              return (
+                <div
+                  key={skinName}
+                  className={`relative bg-gray-700 py-2 gap-2 flex flex-col items-center justify-center rounded-lg cursor-pointer ${
+                    isOwned ? "border-2 border-blue-500" : "border-2 border-gray-500"
+                  }`}
+                  onClick={() => handleSkinSelect(skinName as SkinName)}
+                >
+                  <Image src={skinsData[skinName as SkinName].image} width={50} height={100} alt={skinName} />
+                  <h4 className="text-xs text-center">{skinsData[skinName as SkinName].title}</h4>
+                  {!isOwned && (
+                    <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
+                      <Image src={Lock} width={20} height={20} alt="Locked" />
+                    </div>
+                  )}
+                  {selectedSkin === skinName && <div className="absolute top-0 right-0 bg-blue-500 text-white text-xs rounded-full p-1">✓</div>}
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
@@ -214,7 +227,7 @@ const Skin: React.FC<SkinProps> = ({
           <div className="bg-neutral-800 border-t-2 border-amber-600 p-6 rounded-t-[46px] top-glow">
             <h2 className="text-purple-500 font-bold text-center text-2xl mb-4">Confirm Purchase</h2>
             <p className="text-white mb-4">
-              Are you sure you want to purchase this skin for {skinPrices[pendingPurchase!]} Points?
+              Are you sure you want to purchase this skin for {skinsData[pendingPurchase!].price.toLocaleString()} Points?
             </p>
             <div className="flex gap-2 justify-center">
               <button
